455c6330c077e83155345893110594c8b12cdb43
Arthur Handras <arthur.handras@gmail.com>
2026-03-07 17:53:33 -0500

    Updated code to remove all warnings. the code compiles clean with 0 errors and 0 warnings.

--- src/main/java/frc/robot/commands/leftAuto.java
+++ src/main/java/frc/robot/commands/leftAuto.java
@@ -3,8 +3,6 @@ package frc.robot.commands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.shooterNeoVortex;
